89a28355cc3ca1f3695de5f524c9bb4676f85864
Irene Serrano <iserrano@mills.edu>
2020-09-20 15:15:11 -0700

    Add attackIdentification to plugin

--- src/main/java/io/gitbub/irenenserrano/IrenesFirstPlugin.java
+++ src/main/java/io/gitbub/irenenserrano/IrenesFirstPlugin.java
@@ -1,8 +1,13 @@
 package io.gitbub.irenenserrano;
 
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class IrenesFirstPlugin extends JavaPlugin{
+public final class IrenesFirstPlugin extends JavaPlugin implements Listener{
   @Override
   public void onEnable() {
     getLogger().info("IrenesFirstPlugin.onEnable has been invoked!");
@@ -11,5 +16,23 @@ public final class IrenesFirstPlugin extends JavaPlugin{
   @Override
   public void onDisable() {
     getLogger().info("IrenesFirstPlugin.onDisable has been invoked!");
+  }
+  /*
+   * Returns a message indicating what mob entity is attacking which player
+   *
+   * @param event EntityDamageByEntityEvent
+   */
+  @EventHandler
+  public void attackIdentification(EntityDamageByEntityEvent event) {
+    //create entities that will represent the player and the attacker
+    Player player = (Player) event.getEntity();
+    Player attacker = (Player) event.getDamager();
+
+    //if the player is being attacked then generate a message stating so
+    if (event.getEntity() instanceof Player && event.getDamager() instanceof Player)
+    {
+      //broadcast a message
+      Bukkit.broadcastMessage(player + " is being attacked by "+ attacker);
+    }
   }
 }
